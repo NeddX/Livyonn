e49982104c3e3fe8d61e369f27d2d71c870b7df0
NeddX <nedbeargt@gmail.com>
2022-09-12 11:04:44 -0700

    Small Change

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Requires to at least CMAKE v3.0 or HIGHER!
 if [ ! -d "builds/" ] 
 then
-    mkdir builds/ && mkdir builds/linux && cd builds/linux && cmake ../ && cd .. && cmake --build builds/linux
+    mkdir builds/ && mkdir builds/linux && cd builds/linux && cmake ../../ && cd .. && cmake --build builds/linux
 else
     cmake --build builds/linux
 fi
